b33ec47628440356238016261e611e71ef91cef8
Benjamin Barkworth <benjaminbarkworth@Benjamins-MacBook-Air.local>
2022-12-01 15:29:48 +0000

    Initial version

--- FizzBuzz/fizzbuzz.py
+++
@@ -1,9 +0,0 @@
-for i in range(1, 101):
-    if i % 15 == 0:
-        print("Fizzbuzz", i)
-    elif i % 5 == 0:
-        print("Buzz", i)
-    elif i % 3 == 0:
-        print("Fizz", i)
-    else:
-        print(i)
